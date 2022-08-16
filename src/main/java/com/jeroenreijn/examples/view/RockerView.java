d17a85ef61c422e7d64ed074fb8dc2fb17d43390
rizsi <andras.schmidt.hu@gmail.com>
2022-08-16 19:02:02 +0200

    Fix rocker benchmark so that it uses its optimized binary implementation

--- src/main/java/com/jeroenreijn/examples/view/RockerView.java
+++ src/main/java/com/jeroenreijn/examples/view/RockerView.java
@@ -1,5 +1,6 @@
 package com.jeroenreijn.examples.view;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Map;
 
@@ -8,27 +9,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.view.AbstractTemplateView;
 
+import com.fizzed.rocker.ContentType;
 import com.fizzed.rocker.Rocker;
+import com.fizzed.rocker.runtime.OutputStreamOutput;
 
 public class RockerView extends AbstractTemplateView {
-
 	@Override
 	protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		String html = Rocker.template("index.rocker.html")
-				.bind("presentations", model.get("presentations"))
-				.bind("i18n", model.get("i18n"))
-				.render()
-				.toString();
-
-		response.setContentLength(html.length());
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
-
-		try (PrintWriter writer = response.getWriter()) {
-			writer.write(html);
-			writer.flush();
+		
+		try (OutputStream os = response.getOutputStream()) {
+			Rocker.template("index.rocker.html")
+					.bind("presentations", model.get("presentations"))
+					.bind("i18n", model.get("i18n"))
+					.render((contentType, charsetName) -> new OutputStreamOutput(contentType, os, charsetName));
 		}
 	}
-
 }
